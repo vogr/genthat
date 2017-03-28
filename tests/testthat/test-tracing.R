f9bd30981c0a51fba8bdb3eb991561d61094904b
Michal Vácha <michal.vacha@live.com>
2017-03-28 20:26:55 +0200

    *added test for params with names enclosed in backticks

--- tests/testthat/test-tracing.R
+++ tests/testthat/test-tracing.R
@@ -21,3 +21,20 @@ test_that("basic trace pushing", {
     }
     expect_equal(counter, 1)
 })
+
+test_that("tracing - names of function parameters enclosed in backticks are supported", {
+    fn1 <- function(a, `b c`, `function`) { a + `b c` + `function` + 1L }
+
+    decorate_function("fn1")
+    fn1(4L, 3L, 2L)
+
+    counter <- 0L
+    while (traces$has_next()) {
+        counter <- counter + 1L
+        trace <- traces$get_next()
+        expect_equal(trace$func, "fn1")
+        expect_equal(trace$args, "structure(list(a=4L,`b c`=3L,`function`=2L), .Names=c(\"a\",\"b c\",\"function\"))")
+        expect_equal(trace$retv, "10L")
+    }
+    expect_equal(counter, 1)
+})
