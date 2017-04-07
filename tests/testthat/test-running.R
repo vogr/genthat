1d2e0072584869fead31a03a5dbed56d9c28b47e
Michal Vácha <michal.vacha@live.com>
2017-04-07 18:50:11 +0200

    *fixing wrong folder

--- tests/testthat/test-running.R
+++ tests/testthat/test-running.R
@@ -7,11 +7,10 @@ context("Running testcases generated from traces")
 
 test_that("gen_tests() and run_generated_tests() basic usage", {
     with_tempdir(function(dir) {
-    	fn1 <- function(a) { a + 1 }
-		fn1 <- decorate_function_val(fn1, "fn1")
-		fn1(42)
-		undecorate_all()
-
+        fn<- function(a) { a + 1 }
+        fn1 <- decorate_functions(fn)
+        fn1[[1]](42)
+           
         res <- gen_tests(dir)
         expect_equal(res$n_success, 1)
         expect_equal(res$n_failed, 0)
