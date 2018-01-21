e2f66df09c2af53a69184e8555047c91806a6ed6
Filip Krikava <krikava@gmail.com>
2018-01-21 21:56:22 +0100

    Update debug messages for running code coverage

--- R/coverage.R
+++ R/coverage.R
@@ -35,22 +35,34 @@ compute_tests_coverage <- function(path, tests, quiet=TRUE) {
 #' @importFrom covr percent_coverage
 #
 do_compute_tests_coverage <- function(tests) {
-    # TODO: accessing the covr:::.coverage is a bit of a hack, yet the most
-    # effective way how to do get increasing coverage per tests
+    # accessing the covr:::.coverage is a bit of a hack, yet the most effective
+    # way how to do get increasing coverage per tests
+
+    n <- length(tests)
+    i <- 1
 
     lapply(tests, function(test) {
         old_coverage <- list2env(as.list(covr:::.counters), env=new.env(parent=emptyenv()))
 
+        log_debug("Running ", i, "/", n, ":", test)
+        i <<- i + 1
+
         tryCatch({
             time <- genthat:::stopwatch(genthat::test_generated_file(test))
-            coverage <- structure(as.list(covr:::.counters), class="coverage")
+            time <- as.numeric(time, units="secs")
 
-            c(covr::percent_coverage(coverage), as.numeric(time, units="secs"))
+            coverage <- structure(as.list(covr:::.counters), class="coverage")
+            coverage <- covr::percent_coverage(coverage)
+
+            log_debug("Finished ", test, " in ", time, " coverage ", coverage)
+
+            c(coverage, time)
         }, error=function(e) {
             # we do not want to capture coverage of failed tests
             # TODO: again not the nicest, we should ask for API to manipulate coverage
             assignInNamespace(".counters", old_coverage, ns="covr")
 
+            log_debug("Failed ", test)
             as_chr_scalar(e$message)
         })
     })
